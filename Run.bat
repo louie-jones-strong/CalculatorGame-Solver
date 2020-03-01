56596215c5f4f3cb7797e99cd0ed1cfa77dfc041
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2020-03-01 18:50:44 +0000

    start hooking up unit tests

---
+++ Run.bat
@@ -0,0 +1,1 @@
+python GameSolver.py
--- UnitTests.py
+++ UnitTests.py
@@ -3,14 +3,28 @@ import Operations
 
 
 def TestOperations():
-    
+    #Add
+    add = Operations.Add(1)
+    assert add.DoAction(1) == 2
+
     return
 
 def TestSolves():
     #load test data
+    testDataList = []
+
     #Run tests
+    for testData in testDataList:
+        found, steps = GameSolver.Solve(testDataList[0], testData[1], testData[2], testData[3])
+
+        if found != testData[4]:
+            exit(code=1)
+
+        if found:
+            exit(code=1)
     return
 
 if __name__ == "__main__":
     TestOperations()
     TestSolves()
+    input("")
