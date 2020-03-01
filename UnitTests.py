1c6fb3964ff6f86ff694612e899105ad74671d08
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2020-03-01 16:01:03 +0000

    trying to hook up circle ci

---
+++ UnitTests.py
@@ -0,0 +1,16 @@
+import GameSolver
+import Operations
+
+
+def TestOperations():
+    
+    return
+
+def TestSolves():
+    #load test data
+    #Run tests
+    return
+
+if __name__ == "__main__":
+    TestOperations()
+    TestSolves()
