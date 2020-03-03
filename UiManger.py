2a006d257ee096b768a4e1a09dc50e32da6add01
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2020-03-03 00:14:05 +0000

    hook up fade

--- UiManger.py
+++ UiManger.py
@@ -2,23 +2,41 @@ from pygame import display, draw, Color, gfxdraw, mouse
 import pygame
 import os
 
-class UiButton:#todo make this a sprite to speed it up
+class UiPiece:#todo make this a sprite to speed it up
 
-	def __init__(self, pos, size, normalImage=None, hoverImage=None, pressImage=None):
+	def __init__(self, pos, size, normalImage=None):
 		self.Pos = pos
 		self.Size = size
 		self.NormalImage = normalImage
-		self.HoverImage = hoverImage
-		self.PressImage = pressImage
+		self.HoverImage = None
+		self.PressImage = None
+		self.OnClick = None
+		self.FadedImage = None
+		self.GetIsFade = None
 
 		if self.NormalImage != None:
-			self.NormalImage = pygame.transform.scale(self.NormalImage, size)
-		if self.HoverImage != None:
-			self.HoverImage = pygame.transform.scale(self.HoverImage, size)
-		if self.PressImage != None:
-			self.PressImage = pygame.transform.scale(self.PressImage, size)
+			self.NormalImage = pygame.transform.scale(self.NormalImage, self.Size)
 		return
 
+	def SetUpButton(self, hoverImage=None, pressImage=None, onClick=None):
+		self.HoverImage = hoverImage
+		self.PressImage = pressImage
+		self.OnClick = onClick
+
+		if self.HoverImage != None:
+			self.HoverImage = pygame.transform.scale(self.HoverImage, self.Size)
+		if self.PressImage != None:
+			self.PressImage = pygame.transform.scale(self.PressImage, self.Size)
+		return
+
+	def SetUpFade(self, fadedImage, getIsFade):
+		self.FadedImage = fadedImage
+		self.GetIsFade = getIsFade
+
+		if self.FadedImage != None:
+			self.FadedImage = pygame.transform.scale(self.FadedImage, self.Size)
+		return
+		
 	def Update(self, screen, debugMode):
 		pos = mouse.get_pos()
 		mouseOverButton = (pos[0] > self.Pos[0] and pos[0] < self.Pos[0] + self.Size[0] and
@@ -27,10 +45,17 @@ class UiButton:#todo make this a sprite to speed it up
 		mouseDown = mouse.get_pressed()[0]
 
 		if mouseOverButton and mouseDown:
+			if self.OnClick != None:
+				self.OnClick()
+
 			if self.PressImage != None:
 				screen.blit(self.PressImage, self.Pos)
 			elif self.NormalImage != None:
 				screen.blit(self.NormalImage, self.Pos)
+		
+		elif self.GetIsFade != None and self.GetIsFade():
+			if self.FadedImage != None:
+				screen.blit(self.FadedImage, self.Pos)
 
 		elif mouseOverButton and not mouseDown:
 			if self.HoverImage != None:
@@ -38,7 +63,7 @@ class UiButton:#todo make this a sprite to speed it up
 			elif self.NormalImage != None:
 				screen.blit(self.NormalImage, self.Pos)
 
-		elif not mouseOverButton:
+		else:
 			if self.NormalImage != None:
 				screen.blit(self.NormalImage, self.Pos)
 
@@ -49,8 +74,10 @@ class UiButton:#todo make this a sprite to speed it up
 
 class UiManger:
 	ImageCache = {}
-	ButtonList = []
+	PieceList = []
 	DebugMode = False
+	SolarCovered = False
+	MouseStartPos = None
 
 	def LoadImage(self, imageName, scaleFactor=1):
 		if imageName not in self.ImageCache:
@@ -71,45 +98,101 @@ class UiManger:
 		display.init()
 		pygame.font.init()
 
-		backGround = self.LoadImage("BackGround", 0.35)
-		self.Resolution = backGround.get_size()
+		self.BackGround = self.LoadImage("BackGround", 0.35)
+		self.Resolution = self.BackGround.get_size()
 
 		#window
 		self.Window = display.set_mode(self.Resolution)
+		return
+
+	def Update(self):
+		if not self.Running:
+			return
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				self.Quit()
+				return
+
+		self.Window.blit(self.BackGround, [0, 0])
+
+		for button in self.PieceList:
+			button.Update(self.Window, self.DebugMode)
 		
-		self.Window.blit(backGround, [0,0])
+		if self.DebugMode:
+			if mouse.get_pressed()[0]:
+				if self.MouseStartPos == None:
+					self.MouseStartPos = list(mouse.get_pos())
+					
+				size = [mouse.get_pos()[0]-self.MouseStartPos[0],
+						mouse.get_pos()[1]-self.MouseStartPos[1]]
+				print("mouse Pos / Size: " + str(self.MouseStartPos) + ", " + str(size))
+				
+			else:
+				self.MouseStartPos = None
+
+			
+			
+
+		display.update()
+
+		self.SolarCovered = False
 		return
 
-	def Run(self):
-		while self.Running:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					pygame.quit()
-					self.Running = False
-					return False
-
-			for button in self.ButtonList:
-				button.Update(self.Window, self.DebugMode)
-
-			if self.DebugMode and mouse.get_pressed()[0]:
-				print("mouse pos: "+str(mouse.get_pos()))
-
-			display.update()
+	def Quit(self):
+		pygame.quit()
+		self.Running = False
 		return
+	
+	def SetSolarCovered(self):
+		self.SolarCovered = True
+		return
+	def GetSolarCovered(self):
+		return self.SolarCovered
 
+	def SetMainScreen(self):
+		manger.PieceList = []
+
+		piece = UiPiece([220, 30], [105, 35])
+		piece.SetUpButton(onClick=self.SetSolarCovered)
+		manger.PieceList += [piece]
+
+		piece = UiPiece([40, 90], [90, 50], manger.LoadImage("FunGuy_Normal"))
+		piece.SetUpFade(manger.LoadImage("FunGuy_Faded"), self.GetSolarCovered)
+		manger.PieceList += [piece]
+
+		piece = UiPiece([140, 90], [90, 50], manger.LoadImage("TopStats_Normal"))
+		piece.SetUpFade(manger.LoadImage("TopStats_Faded"), self.GetSolarCovered)
+		manger.PieceList += [piece]
+
+		piece = UiPiece([245, 90], [90, 50], manger.LoadImage("TopStats_Normal"))
+		piece.SetUpFade(manger.LoadImage("TopStats_Faded"), self.GetSolarCovered)
+		manger.PieceList += [piece]
+
+		piece = UiPiece([20, 375], [113, 100])
+		manger.PieceList += [piece]
+		piece = UiPiece([133, 375], [113, 100])
+		manger.PieceList += [piece]
+		piece = UiPiece([246, 375], [113, 100])
+		manger.PieceList += [piece]
+
+		manger.PieceList += [UiPiece([20, 485], [113, 100])]
+		manger.PieceList += [UiPiece([133, 485], [113, 100])]
+		manger.PieceList += [UiPiece([246, 485], [113, 100])]
+
+		manger.PieceList += [UiPiece([20, 595], [113, 100],
+                               manger.LoadImage("Button"))]
+		manger.PieceList += [UiPiece([133, 595], [113, 100])]
+		manger.PieceList += [UiPiece([246, 595], [113, 100])]
+		return
 
 if __name__ == "__main__":
 	try:
 		manger = UiManger()
 		manger.DebugMode = True
+		manger.SetMainScreen()
+		while manger.Running:
+			manger.Update()
 
-		manger.ButtonList += [UiButton([20,375], [110,100], 
-			manger.LoadImage("Button"), 
-			manger.LoadImage("Button_Hover"), 
-			manger.LoadImage("Button_Pressed"))]
-
-		manger.ButtonList += [UiButton([220,30], [105,35])]
-
-		manger.Run()
 	except:
 		input("error:")
