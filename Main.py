c874b005966b8a8086d6335d140b93db4454a2ae
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2020-03-22 03:28:41 +0000

    hook up grid button maker so all grids look the same

--- Main.py
+++ Main.py
@@ -32,12 +32,12 @@ class Main:
 		self.SolarCovered = False
 		return self.Manger.Update()
 
+# ui called functions
 	def SetSolarCovered(self):
 		self.SolarCovered = True
 		return
 	def GetSolarCovered(self):
 		return self.SolarCovered
-	
 	def UpdateMovesNum(self, moves):
 		self.Moves = int(moves)
 		return
@@ -47,7 +47,6 @@ class Main:
 	def UpdateStartingNum(self, startingNum):
 		self.StartingNum = int(startingNum)
 		return
-
 	def ClickedSolve(self):
 		if self.DebugMode:
 			print("Sovle Clicked")
@@ -90,7 +89,6 @@ class Main:
 			
 		self.SetUpMainScreen()
 		return
-	
 	def ChangeVolume(self, delta):
 		volume = self.AudioPlayer.Volume + delta
 
@@ -103,7 +101,6 @@ class Main:
 		self.AudioPlayer.Volume = volume
 		self.SetupSettingsScreen()
 		return
-
 	def ChangeLevelSelect(self, delta):
 		level = self.Level + delta
 		if level < 0:
@@ -113,12 +110,70 @@ class Main:
 
 		self.SetupSettingsScreen()
 		return
+	def DebugModeToggle(self):
+		self.DebugMode = not self.DebugMode
+		self.Manger.DebugMode = self.DebugMode
+		self.AudioPlayer.DebugMode = self.DebugMode
+		self.SetupSettingsScreen()
+		return
+	def ClearClicked(self):
+		self.StartingNum = 0
+		self.Goal = 0
+		self.Moves = 0
+		self.OperationsList = []
+		self.SolveOrder = []
+		for loop in range(5):
+			self.OperationsList += [Operations.MakeOperation(0)]
+			self.SolveOrder += [""]
 
+		self.SetUpMainScreen()
+		return
+	def SetOperation(self, gridIndex):
+		if self.DebugMode:
+			print("set SetOperation: ["+str(self.OperationSetUpIndex)+"] to " + str(gridIndex))
+
+		op = Operations.MakeOperation(gridIndex)
+		if op == None:
+			self.OperationsList[self.OperationSetUpIndex] = Operations.MakeOperation(0)
+			self.SetUpMainScreen()
+
+		else:
+			self.OperationsList[self.OperationSetUpIndex] = op
+			self.SetUpOperationInfoScreen()
+
+		return
+	def UpdateSetting1(self, value):
+		op = self.OperationsList[self.OperationSetUpIndex]
+		op.SetSetting(0, value)
+		self.SetUpOperationInfoScreen(clearSelected=False)
+		return
+	def UpdateSetting2(self, value):
+		op = self.OperationsList[self.OperationSetUpIndex]
+		op.SetSetting(1, value)
+		self.SetUpOperationInfoScreen(clearSelected=False)
+		return
+
+#end of ui called funtions
+
+	def MakeGridPiece(self, xIndex, yIndex, image=None, yStart=375):
+		xStart = 20
+		
+		boxWidth = 110
+		boxHight = 100
+		xSpacing = 5
+		ySpacing = 10
+
+		x = (boxWidth+xSpacing) * xIndex + xStart
+		y = (boxHight+ySpacing) * yIndex + yStart
+
+		return Piece.UiPiece([x, y], [boxWidth, boxHight], image)
+
+#screens 
 	def SetupSettingsScreen(self):
 		self.SetUpShared(False)
 
 		#volume control row 1
-		piece = Piece.UiPiece([20, 375], [113, 100], "Button")
+		piece = self.MakeGridPiece(0, 0, image="Button")
 		piece.SetUpButton(False, "Button_Hover",
                     "Button_Pressed",
 					onClick=self.ChangeVolume, onClickData=-1)
@@ -126,8 +181,7 @@ class Main:
 		piece.SetupAudio("ButtonDown", "ButtonUp")
 		self.Manger.AddPiece(piece, False)
 
-		piece = Piece.UiPiece([133, 375], [113, 100],
-                  "Button_Black")
+		piece = self.MakeGridPiece(1, 0, image="Button_Black")
 		piece.SetUpLabel("Volume", "", xLabelAnchor=0.5, yLabelAnchor=0)
 		self.Manger.AddPiece(piece, False)
 
@@ -135,8 +189,7 @@ class Main:
 		piece.SetUpLabel(str(self.AudioPlayer.Volume), "", xLabelAnchor=0.5, yLabelAnchor=0.5)
 		self.Manger.AddPiece(piece, False)
 
-		piece = Piece.UiPiece([246, 375], [113, 100],
-                  "Button")
+		piece = self.MakeGridPiece(2, 0, image="Button")
 		piece.SetUpButton(False, "Button_Hover",
                     "Button_Pressed",
 					onClick=self.ChangeVolume, onClickData=1)
@@ -145,8 +198,7 @@ class Main:
 		self.Manger.AddPiece(piece, False)
 
 		#level select row 2
-		piece = Piece.UiPiece([20, 490], [113, 100],
-                  "Button")
+		piece = self.MakeGridPiece(0, 1, image="Button")
 		piece.SetUpButton(False, "Button_Hover",
                     "Button_Pressed",
 					onClick=self.ChangeLevelSelect, onClickData=-1)
@@ -154,8 +206,7 @@ class Main:
 		piece.SetupAudio("ButtonDown", "ButtonUp")
 		self.Manger.AddPiece(piece, False)
 
-		piece = Piece.UiPiece([133, 490], [113, 100],
-                  "Button_Black")
+		piece = self.MakeGridPiece(1, 1, image="Button_Black")
 		piece.SetUpLabel("Level", "", xLabelAnchor=0.5, yLabelAnchor=0)
 		self.Manger.AddPiece(piece, False)
 
@@ -163,8 +214,7 @@ class Main:
 		piece.SetUpLabel(self.Level, "", xLabelAnchor=0.5, yLabelAnchor=0.5)
 		self.Manger.AddPiece(piece, False)
 
-		piece = Piece.UiPiece([246, 490], [113, 100],
-                  "Button")
+		piece = self.MakeGridPiece(2, 1, image="Button")
 		piece.SetUpButton(False, "Button_Hover",
                     "Button_Pressed",
 					onClick=self.ChangeLevelSelect, onClickData=1)
@@ -174,8 +224,7 @@ class Main:
 
 
 
-		piece = Piece.UiPiece([20, 595], [113, 100],
-                  "Settings_Button")
+		piece = self.MakeGridPiece(0, 2, image="Settings_Button")
 		piece.SetUpButton(False, "Settings_Hover",
                     "Settings_Pressed",
 					onClick=self.SetUpMainScreen)
@@ -188,33 +237,12 @@ class Main:
 		else:
 			debugButton += "Red"
 
-		piece = Piece.UiPiece([135, 595], [113, 100],
-                  debugButton)
+		piece = self.MakeGridPiece(1, 2, image=debugButton)
 		piece.SetUpButton(False,
 					onClick=self.DebugModeToggle)
 		piece.SetUpLabel("Debug", "", yLabelAnchor=0.5)
 		piece.SetupAudio("ButtonDown", "ButtonUp")
 		self.Manger.AddPiece(piece, False)
-		return
-
-	def DebugModeToggle(self):
-		self.DebugMode = not self.DebugMode
-		self.Manger.DebugMode = self.DebugMode
-		self.AudioPlayer.DebugMode = self.DebugMode
-		self.SetupSettingsScreen()
-		return
-
-	def ClearClicked(self):
-		self.StartingNum = 0
-		self.Goal = 0
-		self.Moves = 0
-		self.OperationsList = []
-		self.SolveOrder = []
-		for loop in range(5):
-			self.OperationsList += [Operations.MakeOperation(0)]
-			self.SolveOrder += [""]
-
-		self.SetUpMainScreen()
 		return
 
 	def SetUpShared(self, selectable=True, clearSelected=True):
@@ -257,8 +285,7 @@ class Main:
 		self.SetUpShared()
 		#button Grid
 		#row 1
-		piece = Piece.UiPiece([20, 375], [113, 100],
-                  "Button")
+		piece = self.MakeGridPiece(0, 0, image="Button")
 		piece.SetUpButton(False, "Button_Hover",
                     "Button_Pressed",
 					onClick=self.ClickedSolve,
@@ -268,7 +295,7 @@ class Main:
 		self.Manger.AddPiece(piece, True)
 
 		op = self.OperationsList[0]
-		piece = Piece.UiPiece([133, 375], [113, 100], op.BaseImage)
+		piece = self.MakeGridPiece(1, 0, image=op.BaseImage)
 		piece.SetUpButton(False, onClick=self.SetUpOperationSelectScreen, onClickData=0)
 		piece.SetUpLabel(op.ToString(), "", xLabelAnchor=0.5, yLabelAnchor=0.5)
 		piece.SetupAudio("ButtonDown", "ButtonUp")
@@ -278,8 +305,7 @@ class Main:
 		piece.SetUpLabel(self.SolveOrder[0], "", xLabelAnchor=1, yLabelAnchor=0.5)
 		self.Manger.AddPiece(piece, False)
 
-		piece = Piece.UiPiece([246, 375], [113, 100],
-                  "Button_Red")
+		piece = self.MakeGridPiece(2, 0, image="Button_Red")
 		piece.SetUpButton(False, "Button_Red_Hover",
                     "Button_Red_Pressed",
 					onClick=self.ClearClicked)
@@ -288,11 +314,9 @@ class Main:
 		self.Manger.AddPiece(piece, False)
 
 		#row 2
-		piece = Piece.UiPiece([20, 485], [113, 100])
-		self.Manger.AddPiece(piece, False)
 
 		op = self.OperationsList[1]
-		piece = Piece.UiPiece([133, 485], [113, 100], op.BaseImage)
+		piece = self.MakeGridPiece(1, 1, image=op.BaseImage)
 		piece.SetUpButton(False, onClick=self.SetUpOperationSelectScreen, onClickData=1)
 		piece.SetUpLabel(op.ToString(), "", xLabelAnchor=0.5, yLabelAnchor=0.5)
 		piece.SetupAudio("ButtonDown", "ButtonUp")
@@ -304,7 +328,7 @@ class Main:
 		self.Manger.AddPiece(piece, False)
 
 		op = self.OperationsList[2]
-		piece = Piece.UiPiece([246, 485], [113, 100], op.BaseImage)
+		piece = self.MakeGridPiece(2, 1, image=op.BaseImage)
 		piece.SetUpButton(False, onClick=self.SetUpOperationSelectScreen, onClickData=2)
 		piece.SetUpLabel(op.ToString(), "", xLabelAnchor=0.5, yLabelAnchor=0.5)
 		piece.SetupAudio("ButtonDown", "ButtonUp")
@@ -315,8 +339,7 @@ class Main:
 		self.Manger.AddPiece(piece, False)
 
 		#row 3
-		piece = Piece.UiPiece([20, 595], [113, 100],
-                  "Settings_Button")
+		piece = self.MakeGridPiece(0, 2, image="Settings_Button")
 		piece.SetUpButton(False, "Settings_Hover",
                     "Settings_Pressed",
 					onClick=self.SetupSettingsScreen)
@@ -324,7 +347,7 @@ class Main:
 		self.Manger.AddPiece(piece, False)
 
 		op = self.OperationsList[3]
-		piece = Piece.UiPiece([133, 595], [113, 100], op.BaseImage)
+		piece = self.MakeGridPiece(1, 2, image=op.BaseImage)
 		piece.SetUpButton(False, onClick=self.SetUpOperationSelectScreen, onClickData=3)
 		piece.SetUpLabel(op.ToString(), "", xLabelAnchor=0.5, yLabelAnchor=0.5)
 		piece.SetupAudio("ButtonDown", "ButtonUp")
@@ -335,7 +358,7 @@ class Main:
 		self.Manger.AddPiece(piece, False)
 
 		op = self.OperationsList[4]
-		piece = Piece.UiPiece([246, 595], [113, 100], op.BaseImage)
+		piece = self.MakeGridPiece(2, 2, image=op.BaseImage)
 		piece.SetUpButton(False, onClick=self.SetUpOperationSelectScreen, onClickData=4)
 		piece.SetUpLabel(op.ToString(), "", xLabelAnchor=0.5, yLabelAnchor=0.5)
 		piece.SetupAudio("ButtonDown", "ButtonUp")
@@ -347,21 +370,6 @@ class Main:
 
 		return
 	
-	def SetOperation(self, gridIndex):
-		if self.DebugMode:
-			print("set SetOperation: ["+str(self.OperationSetUpIndex)+"] to " + str(gridIndex))
-
-		op = Operations.MakeOperation(gridIndex)
-		if op == None:
-			self.OperationsList[self.OperationSetUpIndex] = Operations.MakeOperation(0)
-			self.SetUpMainScreen()
-
-		else:
-			self.OperationsList[self.OperationSetUpIndex] = op
-			self.SetUpOperationInfoScreen()
-
-		return
-
 	def SetUpOperationSelectScreen(self, gridIndex):
 		if self.DebugMode:
 			print("Setup Operation Screen index: " + str(gridIndex))
@@ -370,10 +378,9 @@ class Main:
 		self.SetUpShared(False)
 
 		#button Grid
-		xStart = 20
 		yStart = 265
 
-		piece = Piece.UiPiece([xStart-10, yStart-10], [360, 450],
+		piece = Piece.UiPiece([10, yStart-10], [360, 450],
 			"Popup_BackGround")
 		self.Manger.AddPiece(piece, False)
 
@@ -384,15 +391,13 @@ class Main:
 				op = Operations.MakeOperation(loop)
 				
 				if op != None:
-					piece = Piece.UiPiece([xStart+x*115, yStart+y*110], [110, 100],
-						op.BaseImage)
+					piece = self.MakeGridPiece(x, y, image=op.BaseImage, yStart=yStart)
 					piece.SetUpButton(False, onClick=self.SetOperation, onClickData=loop)
 					piece.SetUpLabel(op.ToString(), "", xLabelAnchor=0.5, yLabelAnchor=0.5)
 					piece.SetupAudio("ButtonDown", "ButtonUp")
 					self.Manger.AddPiece(piece, False)
 				else:
-					piece = Piece.UiPiece([xStart+x*115, yStart+y*110], [110, 100],
-						"Button_Red")
+					piece = self.MakeGridPiece(x, y, image="Button_Red", yStart=yStart)
 					piece.SetUpButton(False, "Button_Red_Hover",
 						"Button_Red_Pressed",
 						onClick=self.SetOperation, 
@@ -404,18 +409,6 @@ class Main:
 
 
 				loop += 1
-		return
-
-	def UpdateSetting1(self, value):
-		op = self.OperationsList[self.OperationSetUpIndex]
-		op.SetSetting(0, value)
-		self.SetUpOperationInfoScreen(clearSelected=False)
-		return
-
-	def UpdateSetting2(self, value):
-		op = self.OperationsList[self.OperationSetUpIndex]
-		op.SetSetting(1, value)
-		self.SetUpOperationInfoScreen(clearSelected=False)
 		return
 
 	def SetUpOperationInfoScreen(self, clearSelected=True):
@@ -464,7 +457,7 @@ class Main:
 			piece.SetupAudio("ButtonDown", "ButtonUp")
 			self.Manger.AddPiece(piece, True)
 		return
-
+#end of screens
 
 if __name__ == "__main__":
 	try:
