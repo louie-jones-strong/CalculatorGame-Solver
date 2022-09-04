8eb2dff01a8429bbc567d2f5139f15d6d0e969d9
Louie Jones-Strong <louiej-s@hotmail.co.uk>
2022-09-04 11:33:34 +0100

    update unit testing to unittest lib

--- UnitTests.py
+++ UnitTests.py
@@ -1,3 +1,5 @@
+import unittest
+
 import Solver.GameSolver as GameSolver
 import Solver.Operations as Operations
 import Solver.LevelData as LevelData
@@ -5,150 +7,79 @@ import json
 import os
 import traceback
 
-class UnitTests:
+class UnitTests(unittest.TestCase):
 
-	def __init__(self):
-		self.NumFailed = 0
-		self.TestNumber = 1
-		self.GroupText = ""
-		self.GroupStart = 1
-		self.GroupPassed = True
-		self.GroupName = ""
-		self.Sovler = GameSolver.GameSolver()
+	# def __init__(self):
+	# 	self.Sovler = GameSolver.GameSolver()
 
-		try:
-			self.TestOperations()
+	# 	return
 
-			if self.NumFailed == 0:
-				self.TestSolves()
+# test each operation
 
-		except Exception as e:
-			self.AssertEqual(False, True, "working code")
-			self.SetGroup("test: "+str(self.TestNumber)+")")
-			
-			strTrace = traceback.format_exc()
-
-			print(strTrace)
-			
-
-		self.SetGroup("Done")
-
-		print("")
-		print("Number Of Tests Failed: "+str(self.NumFailed))
-		if self.NumFailed > 0:
-			exit(code=1)
-		return
-
-	def SetGroup(self, groupName):
-
-		if len(self.GroupText) > 0:
-			text = str(self.GroupStart)+"-"+str(self.TestNumber-1)+") "
-
-			if self.GroupPassed:
-				text += "Passed "+str(self.GroupName)
-				print(text)
-			else:
-				text += "Failed "+str(self.GroupName)
-				print(text)
-				print(self.GroupText)
-
-
-		self.GroupText = ""
-		self.GroupStart = self.TestNumber
-		self.GroupName = groupName
-		self.GroupPassed = True
-		return
-
-	def AssertEqual(self, value, expectedValue, testName=""):
-		self.GroupText += "\t"+str(self.TestNumber) + ") "
-
-		if value == expectedValue:
-			self.GroupText += "Passed " + str(testName) + "\n"
-		else:
-			self.GroupPassed = False
-			self.NumFailed += 1
-			self.GroupText += "Failed " + str(testName) + "\n"
-
-			expectedString = str(expectedValue)
-			gotString = str(value)
-
-			if type(expectedValue) == list:
-				expectedString = self.OpListToText(expectedValue)
-
-			if type(value) == list:
-				gotString = self.OpListToText(value)
-
-			self.GroupText += "\t\texpected: " + expectedString + " but got " + gotString + "\n"
-
-		self.TestNumber += 1
-		return value == expectedValue
-
-	def TestOperations(self):
-		self.SetGroup("Base Operation")
+	def test_Base_Operation(self):
 		op = Operations.MakeOperation(0)
-		self.AssertEqual(type(op), Operations.Operation, "op Type Check")
-		self.AssertEqual(op.OperationId, 0, "ID Check")
+		self.assertEqual(type(op), Operations.Operation, "op Type Check")
+		self.assertEqual(op.OperationId, 0, "ID Check")
 
-		self.SetGroup("Add")
+	def test_Add(self):
 		op = self.SharedOpTests(1, Operations.Add, "+1", [1])
-		self.AssertEqual(op.DoActionOnValue(1), 2, "+1 DoActionOnValue()")
+		self.assertEqual(op.DoActionOnValue(1), 2, "+1 DoActionOnValue()")
 
 		op = self.SharedOpTests(1, Operations.Add, "-1", [-1])
-		self.AssertEqual(op.DoActionOnValue(1), 0, "-1 DoActionOnValue()")
+		self.assertEqual(op.DoActionOnValue(1), 0, "-1 DoActionOnValue()")
 
-		self.SetGroup("Multiply")
+	def test_Multiply(self):
 		op = self.SharedOpTests(2, Operations.Multiply, "X3", [3])
-		self.AssertEqual(op.DoActionOnValue(1), 3, "3 DoActionOnValue()")
+		self.assertEqual(op.DoActionOnValue(1), 3, "3 DoActionOnValue()")
 
-		self.SetGroup("Divide")
+	def test_Divide(self):
 		op = self.SharedOpTests(3, Operations.Divide, "/3", [3])
-		self.AssertEqual(op.DoActionOnValue(9), 3, "3 DoActionOnValue()")
+		self.assertEqual(op.DoActionOnValue(9), 3, "3 DoActionOnValue()")
 
-		self.SetGroup("Shift")
+	def test_Shift(self):
 		op = self.SharedOpTests(4, Operations.BitShiftRight, "<<")
-		self.AssertEqual(op.DoActionOnValue(10), 1, "right DoActionOnValue()")
+		self.assertEqual(op.DoActionOnValue(10), 1, "right DoActionOnValue()")
 
-		self.SetGroup("Insert")
+	def test_Insert(self):
 		op = self.SharedOpTests(5, Operations.Insert, "12", [12])
-		self.AssertEqual(op.DoActionOnValue(1), 112, "DoActionOnValue()")
+		self.assertEqual(op.DoActionOnValue(1), 112, "DoActionOnValue()")
 
-		self.SetGroup("Translate")
+	def test_Translate(self):
 		op = self.SharedOpTests(6, Operations.Translate, "1=>2", ["1", "2"])
-		self.AssertEqual(op.DoActionOnValue(121), 222, "DoActionOnValue()")
+		self.assertEqual(op.DoActionOnValue(121), 222, "DoActionOnValue()")
 
-		self.SetGroup("Pow")
+	def test_Pow(self):
 		op = self.SharedOpTests(7, Operations.Pow, "Pow 2", [2])
-		self.AssertEqual(op.DoActionOnValue(4), 16, "DoActionOnValue()")
+		self.assertEqual(op.DoActionOnValue(4), 16, "DoActionOnValue()")
 
 		op = self.SharedOpTests(7, Operations.Pow, "Pow 3", [3])
-		self.AssertEqual(op.DoActionOnValue(4), 64, "DoActionOnValue()")
+		self.assertEqual(op.DoActionOnValue(4), 64, "DoActionOnValue()")
 
-		self.SetGroup("Flip")
+	def test_Flip(self):
 		op = self.SharedOpTests(8, Operations.Flip, "+/-")
-		self.AssertEqual(op.DoActionOnValue(1), -1, "DoActionOnValue()")
+		self.assertEqual(op.DoActionOnValue(1), -1, "DoActionOnValue()")
 
-
-		self.SetGroup("Reverse")
+	def test_Reverse(self):
 		op = self.SharedOpTests(9, Operations.Reverse, "Reverse")
-		self.AssertEqual(op.DoActionOnValue(1234), 4321, "DoActionOnValue()")
-		
-		self.SetGroup("Sum")
+		self.assertEqual(op.DoActionOnValue(1234), 4321, "DoActionOnValue()")
+
+	def test_Sum(self):
 		op = self.SharedOpTests(10, Operations.Sum, "Sum")
-		self.AssertEqual(op.DoActionOnValue(1234), 10, "DoActionOnValue()")
+		self.assertEqual(op.DoActionOnValue(1234), 10, "DoActionOnValue()")
 
-		self.SetGroup("SwapLeftOrder")
+	def test_SwapLeftOrder(self):
 		op = self.SharedOpTests(11, Operations.SwapLeftOrder, "Shift <")
-		self.AssertEqual(op.DoActionOnValue(1123), 1231, "DoActionOnValue()")
+		self.assertEqual(op.DoActionOnValue(1123), 1231, "DoActionOnValue()")
 
-		self.SetGroup("SwapRightOrder")
+	def test_SwapRightOrder(self):
 		op = self.SharedOpTests(12, Operations.SwapRightOrder, "Shift >")
-		self.AssertEqual(op.DoActionOnValue(122), 212, "DoActionOnValue()")
+		self.assertEqual(op.DoActionOnValue(122), 212, "DoActionOnValue()")
 
-		self.SetGroup("Mirror")
+	def test_Mirror(self):
 		op = self.SharedOpTests(13, Operations.Mirror, "Mirror")
-		self.AssertEqual(op.DoActionOnValue(12), 1221, "DoActionOnValue()")
+		self.assertEqual(op.DoActionOnValue(12), 1221, "DoActionOnValue()")
 
-		self.SetGroup("Modifier")
+	def test_Modifier(self):
 		op = self.SharedOpTests(14, Operations.Modifier, "[+] 1", [1])
 		testOpList = [op]
 		exceptedOpList = [op]
@@ -161,9 +92,9 @@ class UnitTests:
 		exceptedOpList += [addOp]
 
 		newOpList = op.DoActionOnOpList(testOpList, 123)
-		self.AssertEqual(newOpList, exceptedOpList, "DoActionOnOpList()")
+		self.assertEqual(newOpList, exceptedOpList, "DoActionOnOpList()")
 
-		self.SetGroup("Store")
+	def test_Store(self):
 		op = self.SharedOpTests(15, Operations.Store, "Store")
 
 		testOpList = [op]
@@ -178,78 +109,41 @@ class UnitTests:
 		exceptedOpList += [addOp]
 
 		newOpList = op.DoActionOnOpList(testOpList, 123)
-		self.AssertEqual(newOpList, exceptedOpList, "DoActionOnOpList()")
+		self.assertEqual(newOpList, exceptedOpList, "DoActionOnOpList()")
 
-		self.SetGroup("Inv10")
+	def test_Inv10(self):
 		op = self.SharedOpTests(16, Operations.Inv10, "Inv10")
-		self.AssertEqual(op.DoActionOnValue(1234567890), 9876543210, "DoActionOnValue()")
+		self.assertEqual(op.DoActionOnValue(1234567890), 9876543210, "DoActionOnValue()")
 
-
-		self.SetGroup("Portals")
+	def test_Portals(self):
 		newNum = GameSolver.DoPortalMoves(100, None, None)
-		self.AssertEqual(newNum, 100, "DoPortalMoves(100, None, None)")
+		self.assertEqual(newNum, 100, "DoPortalMoves(100, None, None)")
 
 		newNum = GameSolver.DoPortalMoves(10, 1, 0)
-		self.AssertEqual(newNum, 1, "DoPortalMoves(10, 1, 0)")
+		self.assertEqual(newNum, 1, "DoPortalMoves(10, 1, 0)")
 
 		newNum = GameSolver.DoPortalMoves(199, 2, 0)
-		self.AssertEqual(newNum, 1, "DoPortalMoves(199, 2, 0)")
+		self.assertEqual(newNum, 1, "DoPortalMoves(199, 2, 0)")
 
 		newNum = GameSolver.DoPortalMoves(991, 2, 0)
-		self.AssertEqual(newNum, 1, "DoPortalMoves(991, 2, 0)")
+		self.assertEqual(newNum, 1, "DoPortalMoves(991, 2, 0)")
 
 		newNum = GameSolver.DoPortalMoves(946, 2, 1)
-		self.AssertEqual(newNum, 46, "DoPortalMoves(946, 2, 1)")
+		self.assertEqual(newNum, 46, "DoPortalMoves(946, 2, 1)")
 
 		newNum = GameSolver.DoPortalMoves(150, 2, 1)
-		self.AssertEqual(newNum, 60, "DoPortalMoves(150, 2, 1)")
+		self.assertEqual(newNum, 60, "DoPortalMoves(150, 2, 1)")
 
 		newNum = GameSolver.DoPortalMoves(964, 2, 1)
-		self.AssertEqual(newNum, 64, "DoPortalMoves(964, 2, 1)")
+		self.assertEqual(newNum, 64, "DoPortalMoves(964, 2, 1)")
 
 		newNum = GameSolver.DoPortalMoves(96, 2, 1)
-		self.AssertEqual(newNum, 96, "DoPortalMoves(96, 2, 1)")
+		self.assertEqual(newNum, 96, "DoPortalMoves(96, 2, 1)")
 		return
 
-	def SharedOpTests(self, opId, exceptedType, exceptedString, settingList=None):
-		op = Operations.MakeOperation(opId)
-		self.AssertEqual(type(op), exceptedType, "op Type Check")
-		self.AssertEqual(op.OperationId, opId, "ID Check")
 
-		if settingList != None:
-			for index in range(len(settingList)):
-				op.SetSetting(index, settingList[index])
-
-		self.AssertEqual(op.IsValid(), True, "IsVaild()")
-		self.AssertEqual(str(op), exceptedString, "str()")
-
-		self.TestOpSetting(op)
-
-		return op
-
-	def TestOpSetting(self, op):
-		for loop in range(len(op.Setting)):
-			item = op.Setting[loop]
-			
-			valueType = type(item.Value())
-			self.AssertEqual(valueType, item.SettingType, "Setting["+str(loop)+"] value type Check")
-		return
-
-	def OpListToText(self, opList):
-		text = "[ "
-
-		text += str(opList[0])
-
-		for index in range(1, len(opList)):
-			
-			text += ", " + str(opList[index])
-
-		text += " ]"
-		return text
-
-	def TestSolves(self):
-		
-		self.SetGroup("Loading Level Data")
+# checking level data
+	def test_Loading_Level_Data(self):
 
 		path = os.getcwd()
 		path = os.path.join(path, "Assets", "Data")
@@ -258,27 +152,64 @@ class UnitTests:
 		levelsDataDict = json.load(file)
 		file.close()
 
-		self.AssertEqual(0 in levelsDataDict, False, "level 0 should not be in level data")
+		self.assertEqual(0 in levelsDataDict, False, "level 0 should not be in level data")
 
 		for loop in range(1, len(levelsDataDict)+1):
 
 			key = str(loop)
 
-			self.SetGroup("Regression Testing Level: "+str(key))
 
-			if self.AssertEqual(key in levelsDataDict, True, "level key in level data"):
+			if self.assertEqual(key in levelsDataDict, True, "level key in level data"):
 
 				levelDataDict = levelsDataDict[key]
 				self.CheckLevelData(key, levelDataDict)
 		return
-	
+
+
+# helpers
+	def SharedOpTests(self, opId, exceptedType, exceptedString, settingList=None):
+		op = Operations.MakeOperation(opId)
+		self.assertEqual(type(op), exceptedType, "op Type Check")
+		self.assertEqual(op.OperationId, opId, "ID Check")
+
+		if settingList != None:
+			for index in range(len(settingList)):
+				op.SetSetting(index, settingList[index])
+
+		self.assertEqual(op.IsValid(), True, "IsVaild()")
+		self.assertEqual(str(op), exceptedString, "str()")
+
+		self.TestOpSetting(op)
+
+		return op
+
+	def TestOpSetting(self, op):
+		for loop in range(len(op.Setting)):
+			item = op.Setting[loop]
+
+			valueType = type(item.Value())
+			self.assertEqual(valueType, item.SettingType, "Setting["+str(loop)+"] value type Check")
+		return
+
+	def OpListToText(self, opList):
+		text = "[ "
+
+		text += str(opList[0])
+
+		for index in range(1, len(opList)):
+
+			text += ", " + str(opList[index])
+
+		text += " ]"
+		return text
+
 	def CheckLevelData(self, key, levelDataDict):
 		levelData = LevelData.LevelData()
 		neededMigration = levelData.Deserialize(levelDataDict)
 
-		self.AssertEqual(neededMigration, False, "LevelData Version Check")
+		self.assertEqual(neededMigration, False, "LevelData Version Check")
 
-		self.AssertEqual(key == str(levelData.Level), True, "key == level")
+		self.assertEqual(key == str(levelData.Level), True, "key == level")
 
 		operationsList = []
 		loop = 0
@@ -287,29 +218,31 @@ class UnitTests:
 			opType = type(op)
 			if opType != Operations.Operation:
 				self.TestOpSetting(op)
-				self.AssertEqual(op.IsValid(), True, "op[" + str(loop) + "] ("+str(opType.__name__)+") isVaild")
-					
+				self.assertEqual(op.IsValid(), True, "op[" + str(loop) + "] ("+str(opType.__name__)+") isVaild")
+
 
 			operationsList += [op]
 			loop += 1
 
-		self.AssertEqual(len(operationsList) <= 5, True, "length of operationsList is smaller or equal to 5")
+		self.assertEqual(len(operationsList) <= 5, True, "length of operationsList is smaller or equal to 5")
 
 		if levelData.PortalFrom == None or levelData.PortalTo == None:
-			self.AssertEqual(levelData.PortalFrom == levelData.PortalTo, True, "portals both none data Check")
+			self.assertEqual(levelData.PortalFrom == levelData.PortalTo, True, "portals both none data Check")
 		else:
-			self.AssertEqual(levelData.PortalFrom > levelData.PortalTo, True, "PortalFrom > PortalTo")
-			self.AssertEqual(levelData.PortalFrom > 0, True, "PortalFrom > 0")
-			self.AssertEqual(levelData.PortalFrom <= 5, True, "PortalFrom <= 5")
-			self.AssertEqual(levelData.PortalTo >= 0, True, "PortalTo >= 0")
-			self.AssertEqual(levelData.PortalTo <= 4, True, "PortalTo <= 4")
+			self.assertEqual(levelData.PortalFrom > levelData.PortalTo, True, "PortalFrom > PortalTo")
+			self.assertEqual(levelData.PortalFrom > 0, True, "PortalFrom > 0")
+			self.assertEqual(levelData.PortalFrom <= 5, True, "PortalFrom <= 5")
+			self.assertEqual(levelData.PortalTo >= 0, True, "PortalTo >= 0")
+			self.assertEqual(levelData.PortalTo <= 4, True, "PortalTo <= 4")
 
 		found, solveOrder = self.Sovler.Solve(levelData)
-		self.AssertEqual(found, True, "Found Solve "+ self.OpListToText(operationsList))
+		self.assertEqual(found, True, "Found Solve "+ self.OpListToText(operationsList))
 		if found:
-			self.AssertEqual(len(solveOrder) > 0, True, "number of min moves")
-			self.AssertEqual(len(solveOrder) <= levelData.Moves, True, "number of moves are valid")
+			self.assertEqual(len(solveOrder) > 0, True, "number of min moves")
+			self.assertEqual(len(solveOrder) <= levelData.Moves, True, "number of moves are valid")
 		return
 
+
+
 if __name__ == "__main__":
-	UnitTests()
+	unittest.main()
